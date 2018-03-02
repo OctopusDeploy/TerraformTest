1583d5a4b430bb2b6ff3cae43166823bd8193644
Matthew Casperson <matthewcasperson@gmail.com>
2018-03-02 12:20:09 +1000

    Added first example

---
+++ SimpleEC2/ec2.tf
@@ -0,0 +1,54 @@
+{
+   "terraform":[
+      {
+         "backend":[
+            {
+               "s3":{
+                  "bucket":"octopus-terraform-demo",
+                  "key":"terraform-02.tfstate",
+                  "region":"us-east-1"
+               }
+            }
+         ]
+      }
+   ],
+   "provider":{
+      "aws":{
+
+      }
+   },
+   "variable":{
+      "ami":{
+         "type":"list",
+         "description":"the AMI to use",
+         "default": "ami-cb9ec1b1"
+      }
+   },
+   "resource":{
+      "aws_instance":{
+         "web":{
+            "ami":"${var.ami}",
+            "instance_type":"m3.medium",
+            "count":1,
+            "source_dest_check":false,
+            "connection":{
+               "user":"ec2-user"
+            }
+         }
+      }
+   },
+   "output":{
+      "test":{
+         "value":"hi there"
+      },
+      "test2":{
+         "value":[
+            "hi there",
+            "hi again"
+         ]
+      },
+      "test3":{
+         "value":"${map(\"a\", \"hi\")}"
+      }
+   }
+}
